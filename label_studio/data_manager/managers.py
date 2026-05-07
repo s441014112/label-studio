835b6528464a576d72fa33074b19acaeda14fab1
qiuzheng <110602270@qq.com>
2026-05-07 15:06:02 +0800

    fix: 1.数据库执行语句兼容mysql8数据库

--- label_studio/data_manager/managers.py
+++ label_studio/data_manager/managers.py
@@ -632,7 +632,7 @@ def annotate_storage_filename(queryset: TaskQuerySet) -> TaskQuerySet:
 
 
 def annotate_annotations_results(queryset):
-    if settings.DJANGO_DB == settings.DJANGO_DB_SQLITE:
+    if settings.DJANGO_DB in (settings.DJANGO_DB_SQLITE, settings.DJANGO_DB_MYSQL):
         return queryset.annotate(
             annotations_results=Coalesce(
                 GroupConcat('annotations__result'), Value(''), output_field=models.CharField()
@@ -643,7 +643,7 @@ def annotate_annotations_results(queryset):
 
 
 def annotate_predictions_results(queryset):
-    if settings.DJANGO_DB == settings.DJANGO_DB_SQLITE:
+    if settings.DJANGO_DB in (settings.DJANGO_DB_SQLITE, settings.DJANGO_DB_MYSQL):
         return queryset.annotate(
             predictions_results=Coalesce(
                 GroupConcat('predictions__result'), Value(''), output_field=models.CharField()
@@ -654,7 +654,7 @@ def annotate_predictions_results(queryset):
 
 
 def annotate_annotators(queryset):
-    if settings.DJANGO_DB == settings.DJANGO_DB_SQLITE:
+    if settings.DJANGO_DB in (settings.DJANGO_DB_SQLITE, settings.DJANGO_DB_MYSQL):
         return queryset.annotate(
             annotators=Coalesce(GroupConcat('annotations__completed_by'), Value(''), output_field=models.CharField())
         )
@@ -690,14 +690,14 @@ def annotate_predictions_score(queryset):
 
 
 def annotate_annotations_ids(queryset):
-    if settings.DJANGO_DB == settings.DJANGO_DB_SQLITE:
+    if settings.DJANGO_DB in (settings.DJANGO_DB_SQLITE, settings.DJANGO_DB_MYSQL):
         return queryset.annotate(annotations_ids=GroupConcat('annotations__id', output_field=models.CharField()))
     else:
         return queryset.annotate(annotations_ids=ArrayAgg('annotations__id', default=Value([])))
 
 
 def annotate_predictions_model_versions(queryset):
-    if settings.DJANGO_DB == settings.DJANGO_DB_SQLITE:
+    if settings.DJANGO_DB in (settings.DJANGO_DB_SQLITE, settings.DJANGO_DB_MYSQL):
         return queryset.annotate(
             predictions_model_versions=GroupConcat('predictions__model_version', output_field=models.CharField())
         )
